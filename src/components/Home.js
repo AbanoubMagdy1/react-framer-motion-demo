3b9db853f8d346aeed7a590778223d1f3fd89e35
AbanoubMagdy1 <bebomagdy666@gmail.com>
2022-09-18 18:47:36 +0200

    Animating home button

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,15 @@ import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import RouteAnimated from './RouteAnimated';
 
+const buttonVariant = {
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {yoyo: Infinity, duration: .4}
+  }
+}
+
 const Home = () => {
   return (
     <RouteAnimated>
@@ -11,7 +20,7 @@ const Home = () => {
           Welcome to Pizza Joint
         </motion.h2>
         <Link to="/base">
-          <motion.button animate={{}}>
+          <motion.button variants={buttonVariant} whileHover="hover">
             Create Your Pizza
           </motion.button>
         </Link>
